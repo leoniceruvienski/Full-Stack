3b6734758982b399b68795a3f7b4f2d15cbf45fd
Leonice <leonice@pop-os.localdomain>
2022-03-10 10:25:43 -0300

    Aula 03

---
+++ 03 Tipos de Parâmetros/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+const app = express()
+
+//  Tipos de Parâmetros
+
+//  Query Params
+//  Route Params
+//  Request Body
+
+app.get('/users', (request, response) => {
+    const query = request.query
+    
+    console.log(query)
+    return response.json({})
+})
