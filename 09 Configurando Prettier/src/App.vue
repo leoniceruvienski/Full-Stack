287af5cd8352c7b14e1ee4d364d4df3e1a54a664
Leonice <leonice@pop-os.localdomain>
2022-05-16 15:53:02 -0300

    aula 09

---
+++ 09 Configurando Prettier/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Hello {{ name }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      name: 'Leonice'
+    }
+  }
+});
+</script>
---
+++ 09 Configurando Prettier/src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import ......App from "./App.vue";
+
+const user = (name) => {}
+
+createApp(App).mount("#app");
+
+// teste
